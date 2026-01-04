f15a36a160bfd9ce27b39e7d090b050d9f8e8afd
mrjayasoorya <mrjayasoorya@gmail.com>
2026-01-04 13:05:57 +0530

    config changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://YOUR_DOMAIN",
+//   site: "https://YOUR_DOMAIN",
   trailingSlash: "always",
-  integrations: [sitemap()]
+  integrations: [sitemap()],
+  base: "/insurance-site/"
 });
